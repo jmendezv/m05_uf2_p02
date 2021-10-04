fba363ac185c113a9d5147200f2857558590865d
pep <jmendez1@xtec.cat>
2021-10-04 20:56:42 +0200

    added arrays and throws

--- src/main/kotlin/cat/copernic/jmendezv/Main.kt
+++ src/main/kotlin/cat/copernic/jmendezv/Main.kt
@@ -1,11 +1,13 @@
 package cat.copernic.jmendezv
 
+import java.lang.ArithmeticException
+import java.lang.IllegalArgumentException
 import kotlin.math.pow
 
 /**
  * El máximo de dos números
  */
-fun maximo(a: Int, b: Int): Int = TODO("Pendiente")
+fun maximo(a: Int, b: Int): Int = if (a > b) a else b
 
 /**
  * Interes simmple = crt
@@ -13,8 +15,7 @@ fun maximo(a: Int, b: Int): Int = TODO("Pendiente")
  * r Tasa de interés en %
  * t tiempo en años
  */
-fun interesSimple(c: Double, r: Double, t: Int) =
-    c * r * t
+fun interesSimple(c: Double, r: Double, t: Int): Double = c * r * t
 
 /**
  * Interés compuesto = c (1 + r) ^ t
@@ -22,8 +23,7 @@ fun interesSimple(c: Double, r: Double, t: Int) =
  * r Tasa de interés en %
  * t tiempo en años
  */
-fun interesCompuesto(c: Double, r: Double, t: Int) =
-    c * (1 + r).pow(t)
+fun interesCompuesto(c: Double, r: Double, t: Int) = c * (1 + r).pow(t)
 
 /**
  * El elemento más pequeño de nums
@@ -31,7 +31,8 @@ fun interesCompuesto(c: Double, r: Double, t: Int) =
  * eg: [2,3,1,4,5,1] = 1
  * eg: [] = IllegalArgumentException
  */
-fun minimo(nums: Array<Int>): Int = TODO("Pendiente")
+fun minimo(nums: Array<Int>): Int =
+    if (nums.isNotEmpty()) nums.sortedArray()[0] else throw IllegalArgumentException()
 
 /**
  * La media de nums
@@ -39,7 +40,7 @@ fun minimo(nums: Array<Int>): Int = TODO("Pendiente")
  * eg: [1,5,6] = 4.0
  * eg: [] = 0
  */
-fun media(nums: Array<Int>): Double = TODO("Pendiente")
+fun media(nums: Array<Int>): Double = nums.average()
 
 /**
  * El elemento central de nums (nums debe estar ordenado)
@@ -48,10 +49,19 @@ fun media(nums: Array<Int>): Double = TODO("Pendiente")
  * eg: [1,2,3] = 2
  *
  * Si hay un número par de elementos:
- * eg: [1,2,3,4,] = (2 + 5) / 2
+ * eg: [1,2,3,4] = (2 + 3) / 2 = 2.5
  *
+ * eg: [] = 0
  */
-fun mediana(nums: Array<Int>): Int = TODO("Pendiente")
+fun mediana(nums: Array<Int>): Double {
+    if (nums.isEmpty()) return 0.0
+    val numsSorted = nums.sortedArray()
+    val N = numsSorted.size
+    return if (N % 2 == 1)
+        numsSorted[N / 2].toDouble()
+    else
+        (numsSorted[N / 2 - 1] + numsSorted[N / 2]) / 2.0
+}
 
 /**
  * El elemento más repetido
@@ -61,4 +71,16 @@ fun mediana(nums: Array<Int>): Int = TODO("Pendiente")
  * eg: [] = IllegalArgumentException
  *
  */
-fun masRepetido(nums: Array<Int>): Int = TODO("Pendiente")
+fun masRepetido(nums: Array<Int>) = nums.groupBy { it }
+    .maxByOrNull { it.value.size }?.key ?: throw IllegalArgumentException()
+
+
+fun division(a: Double, b: Double) = if (b == 0.0)
+    throw ArithmeticException()
+else
+    a / b
+
+fun ordenaArray(a: Array<Int>) = a.sortedArray()
+
+fun main() {
+}
--- src/test/kotlin/cat/copernic/jmendezv/MainKtTest.kt
+++ src/test/kotlin/cat/copernic/jmendezv/MainKtTest.kt
@@ -5,8 +5,17 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import java.lang.ArithmeticException
+import java.time.Duration
 
 internal class MainKtTest {
+    val array1 = arrayOf(2,3,4,9,4,5,6,2,3,1,4,1)
+    val array2 = arrayOf(1,2,3)
+    val array3 = arrayOf(1,2,3,4)
+    val array4 = arrayOf(6)
+    val array5 = arrayOf(4,6)
 
     @BeforeEach
     fun setUp() {
@@ -18,29 +27,67 @@ internal class MainKtTest {
 
     @Test
     fun maximo() {
+        assertEquals(9, cat.copernic.jmendezv.maximo(9,1))
+    }
+
+    @ParameterizedTest
+    @CsvSource("1,1,1", "3,2,3", "9,3,9")
+    fun maximo2(a: Int, b: Int, c: Int) {
+        assertEquals(c, cat.copernic.jmendezv.maximo(a,b))
     }
 
     @Test
     fun interesSimple() {
+        assertEquals(30.0, cat.copernic.jmendezv.interesSimple(1_000.0, 0.01, 3), 1.0e-3)
+    }
+
+    @ParameterizedTest
+    @CsvSource("1_000.0,0.01,1,10.0", "2_000.0,0.015,1,30.0", "3_000.0,0.01,2,60.0")
+    fun interesSimpleTest(c: Double, r: Double, y: Int, t: Double) {
+        assertEquals(t, cat.copernic.jmendezv.interesSimple(c,r,y), 1.0e-1)
     }
 
     @Test
     fun interesCompuesto() {
+        assertEquals(1030.301, cat.copernic.jmendezv.interesCompuesto(1_000.0, 0.01, 3), 1.0e-3)
     }
 
     @Test
-    fun minimo() {
+    fun minimoTest() {
+        assertTimeout(Duration.ofMillis(100)) {
+            assertEquals(1, cat.copernic.jmendezv.minimo(array1))
+        }
+
     }
 
     @Test
     fun media() {
+        assertEquals(3.666, cat.copernic.jmendezv.media(array1), 1.0e-3)
     }
 
     @Test
     fun mediana() {
+        assertEquals(3.5, cat.copernic.jmendezv.mediana(array1), 1.0e-3)
+        assertEquals(2.0, cat.copernic.jmendezv.mediana(array2), 1.0e-3)
+        assertEquals(2.5, cat.copernic.jmendezv.mediana(array3), 1.0e-3)
+        assertEquals(6.0, cat.copernic.jmendezv.mediana(array4), 1.0e-3)
+        assertEquals(5.0, cat.copernic.jmendezv.mediana(array5), 1.0e-3)
     }
 
     @Test
     fun masRepetido() {
+        assertEquals(4, cat.copernic.jmendezv.masRepetido(array1))
+    }
+
+    @Test
+    fun divisionTest() {
+        org.junit.jupiter.api.assertThrows<ArithmeticException> {
+            division(3.0,0.0)
+        }
+    }
+
+    @Test
+    fun ordenaTest() {
+        assertArrayEquals(arrayOf(1,2,3,4,5), ordenaArray(arrayOf(2,1,3,5,4)))
     }
 }
